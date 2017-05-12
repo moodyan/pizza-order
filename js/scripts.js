cb2ae3c2a6443f24ddb836079de449d07943894a
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-05-12 16:31:53 -0700

    IN PROCESS: Debug delivery option form.

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,11 @@ Pizza.prototype.calculatePrice = function() {
   return total;
 };
 
+// Pizza.prototype.addPrices = function() {
+//   var price1 = newPizza.pizzaPrice;
+//   var price2 = newPizza2.pizzaPrice;
+//
+// };
 
 // USER INTERFACE
 
@@ -56,6 +61,55 @@ $(document).ready(function() {
 
     $("#pizza-order").show();
     $("#pizza-order-form").hide();
+    });
 
-  });
+    $("#pick-up").click(function() {
+      $("#pick-up").hide();
+      $("#delivery").hide();
+      $("#new-pizza-button").hide();
+      $(".pick-up").fadeIn("slow");
+    });
+
+    $("#delivery").click(function() {
+      $("#pick-up").hide();
+      $("#delivery").hide();
+      $("#delivery-form").show();
+    });
+
+    // $('#delivery').click(function() {
+    //     $('#deliveryForm').fadeIn();
+    //   })
+    //   $(document).mouseup(function (e) {
+    //     var container = $("#deliveryForm");
+    //
+    //     if (!container.is(e.target) // if the target of the click isn't the container...
+    //         && container.has(e.target).length === 0) // ... nor a descendant of the container
+    //     {
+    //         container.fadeOut();
+    //     }
+    //   });
+
+    var checkForm = function(e)
+  {
+    var form = (e.target) ? e.target : e.srcElement;
+    if(form.name.value == "") {
+      alert("Please enter your Name");
+      form.name.focus();
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+      return;
+    }
+    if(form.email.value == "") {
+      alert("Please enter a valid Email address");
+      form.email.focus();
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+      return;
+    }
+    if(form.message.value == "") {
+      alert("Please enter your comment or question in the Message box");
+      form.message.focus();
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+      return;
+    }
+  };
+
 });
